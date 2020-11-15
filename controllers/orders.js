c865743a167ac587f6740da11250e094e0041b77
James Hernandez <james@j-hernandez.com>
2020-11-15 07:06:36 -0500

    refactor(routes): Creates Route and Controlller modules

---
+++ controllers/orders.js
@@ -0,0 +1,74 @@
+// Global variables for our prototype
+// Orders array for storing and manipulating orders
+var orders = [];
+// Used to assign unique ids to each order
+var primaryId = 1;
+
+exports.findAll = (req, res) => {
+    if (!orders.length) {
+        next();
+    }
+    res.send(orders);
+}
+
+exports.create = (req, res) => {
+    // req.body now represents the actual request body
+    // as a javascript object
+    // allow for food_name, customer_name, quantity
+    orders.push({
+        id: primaryId,
+        food_name: req.body.food_name,
+        customer_name: req.body.customer_name,
+        quantity: req.body.quantity
+    });
+    primaryId++;
+
+    res.status(200).json({
+        message: "Order created succesfully"
+    })
+}
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    // First attempt
+    // let index = orders.indexOf(id);
+    // if (index > -1) {
+    //     orders.splice(index, 1);
+    // }
+
+    let order = orders.find((order) => {
+        return order.id === Number(id)
+    });
+
+    let orderIndex = orders.findIndex((o) => {
+        return o === order;
+    })
+
+    if (orderIndex > -1) {
+        orders.splice(orderIndex, 1);
+    }
+    res.status(200).send(orders);
+}
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    let order = orders.find((order) => {
+        return order.id === Number(id)
+    });
+
+    const filter = req.body.filter || [];
+    if (filter.length) {
+        let newOrder = {};
+        filter.forEach((f) => {
+            // f is going to represent an item in the filter array
+            // setting newOrder.property = whatever the found property on the 
+            // order object
+            // example:
+            // f = customer_name 
+            // newOrder['customer_name'] = order['customer_name'];
+            newOrder[f] = order[f];
+        })
+        res.status(200).send(newOrder);
+    }
+    res.status(200).send(order);
+}
--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-// Global variables for our prototype
-
-// Orders array for storing and manipulating orders
-let orders = [];
-// Used to assign unique ids to each order
-let primaryId = 1;
+// Importing our Order Routes
+const orderRoutes = require('./routes/orders');
 
 // Middleware
 
@@ -15,14 +11,13 @@ let primaryId = 1;
 // run it through JSON.parse(), returning an object that will 
 // be available as req.body
 
-
 app.use(express.json());
 // request body has been parsed
 app.use(express.urlencoded({extended: false}));
 // request body has been url encoded
 
-// HTML Pages
-app.use(express.static('html')); // no match found? next()
+// Static Files
+app.use(express.static('public')); // no match found? next()
 
 // Custom Middleware
 app.use((req, res, next) => {
@@ -30,93 +25,8 @@ app.use((req, res, next) => {
     next();
 })
 
-// ROUTES 
-// Start matching 
-app.get('/orders', (req, res, next) => {
-    if (!orders.length) {
-        next();
-    }
-    res.send(orders);
-})
-
-// routes
-app.post('/orders', (req, res) => {
-    // req.body now represents the actual request body
-    // as a javascript object
-    // allow for food_name, customer_name, quantity
-    orders.push({
-        id: primaryId,
-        food_name: req.body.food_name,
-        customer_name: req.body.customer_name,
-        quantity: req.body.quantity
-    });
-    primaryId++;
-
-    res.status(200).json({
-        message: "Order created succesfully"
-    })
-})
-
-app.delete('/orders/:id', (req, res) => {
-    const id = req.params.id;
-    // First attempt
-    // let index = orders.indexOf(id);
-    // if (index > -1) {
-    //     orders.splice(index, 1);
-    // }
-
-    let order = orders.find((order) => {
-        return order.id === Number(id)
-    });
-
-    let orderIndex = orders.findIndex((o) => {
-        return o === order;
-    })
-
-    if (orderIndex > -1) {
-        orders.splice(orderIndex, 1);
-    }
-    res.status(200).send(orders);
-})
-
-app.get('/orders/:id', (req, res) => {
-    const id = req.params.id;
-    let order = orders.find((order) => {
-        return order.id === Number(id)
-    });
-
-    const filter = req.body.filter || [];
-    if (filter.length) {
-        let newOrder = {};
-        filter.forEach((f) => {
-            // f is going to represent an item in the filter array
-            // setting newOrder.property = whatever the found property on the 
-            // order object
-            // example:
-            // f = customer_name 
-            // newOrder['customer_name'] = order['customer_name'];
-            newOrder[f] = order[f];
-        })
-        res.status(200).send(newOrder);
-    }
-    res.status(200).send(order);
-});
-
-app.get('/search', (req, res) => {
-    const query = req.query.q;
-    res.send(query);
-})
-
-app.get('/index.html', (req, res) => {
-    res.send('Here is the index');
-})
-
-app.get('/pixel.jpg', (req, res) => {
-    // track everything about this person first
-
-    // send back a generated jpeg response
-    res.send(fakeImage);
-})
+// Routes - Orders
+app.use('/orders', orderRoutes);
 
 app.get('*', (req, res) => {
     res.status(404).send('Not found');
